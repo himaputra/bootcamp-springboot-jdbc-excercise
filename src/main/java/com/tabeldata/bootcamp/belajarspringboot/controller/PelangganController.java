98b6541c01a2139903faeb56d17e0ddf7056684a
Himawan Eka Putra <hima.cia008@gmail.com>
2019-03-11 14:51:56 +0700

    Master Pelanggan

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/controller/PelangganController.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/controller/PelangganController.java
@@ -1,5 +1,53 @@
 package com.tabeldata.bootcamp.belajarspringboot.controller;
 
-public class PelangganController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tabeldata.bootcamp.belajarspringboot.dao.PelangganDao;
+import com.tabeldata.bootcamp.belajarspringboot.model.Pelanggan;
+
+@RestController
+@RequestMapping("/api/pelanggan")
+public class PelangganController {
+	@Autowired
+	private PelangganDao dao;
+	
+	@PostMapping("/")
+	public ResponseEntity<Pelanggan> save(@RequestBody Pelanggan pelanggan) {
+		dao.save(pelanggan);
+		return ResponseEntity.ok().build();
+	}
+	
+	@GetMapping("/json/list")
+	public List<Pelanggan> listJson() {
+		return dao.listPelanggan();
+	}
+	
+	@GetMapping("/{nama}")
+	public ResponseEntity<Pelanggan> findByName(@PathVariable("nama") String nama) {
+		Pelanggan pelanggan = dao.findByName(nama);
+		return ResponseEntity.ok(pelanggan);
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<Pelanggan> update(@RequestBody Pelanggan pelanggan) {
+		dao.update(pelanggan);
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Pelanggan> delete(@PathVariable("id") String id) {
+		dao.delete(id);
+		return ResponseEntity.ok().build();
+	}
 }
--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/dao/PelangganDao.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/dao/PelangganDao.java
@@ -1,7 +1,14 @@
 package com.tabeldata.bootcamp.belajarspringboot.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +19,84 @@ import com.tabeldata.bootcamp.belajarspringboot.model.Pelanggan;
 public class PelangganDao {
 	@Autowired
 	private JdbcTemplate jdbc;
-	
+
+	@Transactional
 	public void save(Pelanggan pelanggan) {
 		String SQL = "insert into pelanggan(id, nama, alamat) values (?, ?, ?)";
-		this.jdbc.update(SQL, new PelangganRowMapper());
+		this.jdbc.update(SQL, new InsertPelanggan(pelanggan));
+	}
+	
+	public List<Pelanggan> listPelanggan() {
+		String SQL = "select * from pelanggan";
+		return this.jdbc.query(SQL, new PelangganRowMapper());
+	}
+	
+	public Pelanggan findByName(String nama) {
+		String SQL = "select * from pelanggan where nama = ?";
+		return this.jdbc.queryForObject(SQL, new PelangganRowMapper(), new Object[] {nama});
+	}
+	
+	@Transactional
+	public void update(Pelanggan pelanggan) {
+		String SQL = "update pelanggan set "
+				+ "nama = ?, "
+				+ "alamat = ? "
+				+ "where id = ?";
+		this.jdbc.update(SQL, new UpdatePelanggan(pelanggan));
+	}
+	
+	@Transactional
+	public void delete(String id) {
+		String SQL = "delete from pelanggan where id = ?";
+		this.jdbc.update(SQL, new Object[] {id});
+	}
+
+//	Class
+	public class UpdatePelanggan implements PreparedStatementSetter {
+		final Pelanggan pelanggan;
+		
+		public UpdatePelanggan(Pelanggan pelanggan) {
+			this.pelanggan = pelanggan;
+		}
+
+		@Override
+		public void setValues(PreparedStatement ps) throws SQLException {
+			// TODO Auto-generated method stub
+			ps.setString(1, pelanggan.getNama());
+			ps.setString(2, pelanggan.getAlamat());
+			ps.setString(3, pelanggan.getId());
+		}
+	}
+	
+	public class InsertPelanggan implements PreparedStatementSetter {
+
+		final Pelanggan pelanggan;
+		
+		public InsertPelanggan(Pelanggan pelanggan) {
+			this.pelanggan = pelanggan;
+		}
+		
+		@Override
+		public void setValues(PreparedStatement ps) throws SQLException {
+			// TODO Auto-generated method stub
+			ps.setString(1, pelanggan.getId());
+			ps.setString(2, pelanggan.getNama());
+			ps.setString(3, pelanggan.getAlamat());
+		}
+
+	}
+	
+	private class PelangganRowMapper implements RowMapper<Pelanggan> {
+
+		@Override
+		public Pelanggan mapRow(ResultSet rs, int rowNum) throws SQLException {
+			// TODO Auto-generated method stub
+			Pelanggan aPelanggan = new Pelanggan();
+			aPelanggan.setId(rs.getString("id"));
+			aPelanggan.setNama(rs.getString("nama"));
+			aPelanggan.setAlamat(rs.getString("alamat"));
+			return aPelanggan;
+		}
+		
 	}
 }
